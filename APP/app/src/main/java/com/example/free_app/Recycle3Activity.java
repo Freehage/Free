0aa0e5adfe7df4e5769676b9fbb59730c5e8c2a6
Heesun <heesun19@ewhain.net>
2022-01-30 22:14:30 +0900

    change recycle_category page2

--- APP/app/src/main/java/com/example/free_app/Recycle3Activity.java
+++ APP/app/src/main/java/com/example/free_app/Recycle3Activity.java
@@ -1,22 +1,27 @@
 package com.example.free_app;
 
-import android.app.FragmentManager;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Recycle3Activity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private FragmentManager fragmentManager;
-    private MapFragment mapFragment;
+public class Recycle3Activity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +31,46 @@ public class Recycle3Activity extends AppCompatActivity implements OnMapReadyCal
         getSupportActionBar().setTitle("제로웨이스트샵 찾아보기");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        fragmentManager = getFragmentManager();
-        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        //마커 생성, 알맹상점 리스테이션
-        LatLng location = new LatLng(37.55589221981869, 126.96953356507146);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.title("알맹상점 리스테이션");
-        markerOptions.snippet("제로웨이스트샵");
-        markerOptions.position(location);
+        this.googleMap = googleMap;
+        //37.553779, 126.911598 알맹상점(마포구)
+        LatLng latLng = new LatLng(37.553779, 126.911598);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("알맹상점");
         googleMap.addMarker(markerOptions);
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
-
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+        googleMap.setMyLocationEnabled(true);
+        } else{
+            checkLocationPermissionWithRationale();
+        }
     }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private void checkLocationPermissionWithRationale() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("위치정보")
+                        .setMessage("이 앱을 사용하기 위해서는 위치정보에 접근이 필요합니다. 위치정보 접근을 허용하여 주세요.")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(Recycle3Activity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        }).create().show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
 }
