553ea14f3b428b67c2622b7360fa04a3aa25d749
bansohee <bsh1004664@ewhain.net>
2022-02-12 00:02:52 +0900

    add object detection tap

--- APP/app/src/main/java/com/example/free_app/MainActivity.java
+++ APP/app/src/main/java/com/example/free_app/MainActivity.java
@@ -1,42 +1,36 @@
 package com.example.free_app;
 
-import androidx.annotation.DrawableRes;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageButton;
-import android.widget.ScrollView;
-import androidx.appcompat.widget.SearchView;
-
-import com.bumptech.glide.Glide;
 import com.example.free_app.cardnews.CustomAdapter;
 import com.example.free_app.database.DatabaseHelper;
 import com.example.free_app.model.Product;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
 import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
 
+    // camera
+    private static final int REQUEST_IMAGE_CODE = 101;
+
     public static Context main_context;
     public int card_num = 0;
 
+    private Button photo;
     private Button recycle;
     private ViewPager2 viewPager2;
     private FragmentStateAdapter fragmentStateAdapter;
@@ -98,31 +92,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 재품 촬영 버튼 클릭.
+        photo = (Button) findViewById(R.id.photo);
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_searchob = new Intent(getApplicationContext(), SearchObjectActivity.class);
+                startActivity(intent_searchob);
+                takePicture();
+            }
+        });
 
-
-
-
+        // 재활용 버튼 클릭.
         recycle = (Button) findViewById(R.id.recycle);
         recycle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent_recycle = new Intent(getApplicationContext(), RecycleActivity.class);
                 startActivity(intent_recycle);
-
             }
         });
 
     }
 
     private void initLoadDB(){
-
         DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
         databaseHelper.openDB();
 
         productlist = databaseHelper.getTableData();
         Log.e("TEST",String.valueOf(productlist.size()));
         databaseHelper.close();
+    }
 
+    // camera 사진 찍기.
+    public void takePicture() {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CODE);
+        }
     }
 
 }
---
+++ APP/app/src/main/java/com/example/free_app/SearchObjectActivity.java
@@ -0,0 +1,13 @@
+package com.example.free_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchObjectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afterdetect);
+    }
+}
