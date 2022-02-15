0eff5ac90f03be9c5098784ec200a08827f2c0db
Hyesoo <hyesoo030@gmail.com>
2022-02-15 23:19:56 +0900

    create NMS

--- APP/app/src/main/java/com/example/free_app/AfterDetectActivity.java
+++ APP/app/src/main/java/com/example/free_app/AfterDetectActivity.java
@@ -27,6 +27,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AfterDetectActivity extends AppCompatActivity {
@@ -35,7 +36,7 @@ public class AfterDetectActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CODE = 101;
 
     // tflite model
-    public String MODEL_NAME = "recyclemodel.tflite";
+    public String MODEL_NAME = "best.tflite";
     protected Interpreter tflite;
     private MappedByteBuffer tfliteModel;
     private TensorImage inputImageBuffer;
@@ -54,7 +55,10 @@ public class AfterDetectActivity extends AppCompatActivity {
     TextView result_detail;
     public org.tensorflow.lite.DataType probabilityDataType;
     public float conf;
+    public float[][] class_score = new float[6300][7];
     public float[][] preoutput2;
+    public float CONF = 0.25f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,14 +129,30 @@ public class AfterDetectActivity extends AppCompatActivity {
     // yolo model result 출력.
     private void showresult() {
         try {
+            String result = "";
             labels = FileUtil.loadLabels(this, "classes.txt");
             TensorBuffer preprocess = probabilityProcessor.process(outputProbabilityBuffer);
             float[] preoutput = preprocess.getFloatArray();
             preoutput2 = arr2arr2(preoutput, 12);
-            int output_index = NMS(preoutput2);
-            Log.e("결과", String.valueOf(output_index));
+            NMS(preoutput2);
+            float[] count_arr = new float[7];
+            float max_bbox_conf = preoutput2[4][0];
+            for(int i = 0; i< class_score.length; i++){
+                int class_label = max(class_score[i]);
+                float bbox_conf = preoutput2[4][i];
 
-            result_detail.setText(labels.get(output_index));
+                if(class_score[i][class_label] > 0 & bbox_conf > max_bbox_conf){
+                    max_bbox_conf = bbox_conf;
+                    count_arr[class_label] += 1;
+                    result = labels.get(class_label);
+                    Log.e("class_label", String.valueOf(i)+' '+String.valueOf(class_label)+' '+String.valueOf(max_bbox_conf));
+                }
+            }
+            Log.e("정답", String.valueOf(count_arr[0])+' '+String.valueOf(count_arr[1])+
+                    ' '+String.valueOf(count_arr[2])+' '+String.valueOf(count_arr[3])+' '+
+                    String.valueOf(count_arr[4])+' '+String.valueOf(count_arr[5])+
+                    ' '+String.valueOf(count_arr[6]));
+            result_detail.setText(result);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -140,44 +160,53 @@ public class AfterDetectActivity extends AppCompatActivity {
         }
     }
 
-    private int NMS(float[][] arr) {
-        float max = 0.0f;
-        int label_index = 8;
+    private void NMS(float[][] arr) {
         for(int i=5; i< 12; i++){
-            float a = max(arr[i]);
-            if(a > max & a > 0){
-                max = a;
-                label_index = i-5;
+            int maxIndex = max(arr[i]);
+            class_score[maxIndex][i-5] = arr[i][maxIndex];
+            float[] max_Bbox = {arr[i-5][maxIndex],arr[i-4][maxIndex],arr[i-3][maxIndex],arr[i-2][maxIndex]};
+            ArrayList<Integer> next = Iou(max_Bbox,arr[i-5],arr[i-4],arr[i-3],arr[i-2],i,maxIndex);
+            while(next != null){
+                Integer next_num = next.get(0);
+                float[] next_Bbox = {arr[i-5][next_num],arr[i-4][next_num],arr[i-3][next_num],arr[i-2][next_num]};
+                next = Iou(next_Bbox,arr[i-5],arr[i-4],arr[i-3],arr[i-2],i,next_num);
             }
-            Log.e("레이블", String.valueOf(label_index));
-            //float[] max_Bbox = {arr[i-5][maxIndex],arr[i-4][maxIndex],arr[i-3][maxIndex],arr[i-2][maxIndex]};
-            //Iou(max_Bbox,arr[i-5],arr[i-4],arr[i-3],arr[i-2],i);
         }
-        return label_index;
 
     }
 
-    private void Iou(float[] max_Bbox, float[] x1, float[] y1, float[] x2, float[] y2, int k) {
+    private ArrayList<Integer> Iou(float[] max_Bbox, float[] x1, float[] y1, float[] x2, float[] y2, int k, int max_index) {
         float maxbox_area = (max_Bbox[2] - max_Bbox[0] + 1) * (max_Bbox[3] - max_Bbox[1] + 1);
+        ArrayList<Integer> next_max = null;
         for(int i=0; i< x1.length; i++){
-            float box2_area = (x2[i] - x1[i] + 1) * (y2[i] - y1[i] + 1);
-            float inter_x1 = Math.max(max_Bbox[0],x1[i]);
-            float inter_y1 = Math.max(max_Bbox[1],y1[i]);
-            float inter_x2 = Math.max(max_Bbox[2],x2[i]);
-            float inter_y2 = Math.max(max_Bbox[3],y2[i]);
+            if(preoutput2[k][i] > CONF){
+                if(i != max_index ){
+                    float box2_area = (x2[i] - x1[i] + 1) * (y2[i] - y1[i] + 1);
+                    float inter_x1 = Math.max(max_Bbox[0],x1[i]);
+                    float inter_y1 = Math.max(max_Bbox[1],y1[i]);
+                    float inter_x2 = Math.max(max_Bbox[2],x2[i]);
+                    float inter_y2 = Math.max(max_Bbox[3],y2[i]);
 
-            float w = Math.max(0,inter_x2-inter_x1 +1);
-            float h = Math.max(0,inter_y2-inter_y1+1);
+                    float w = Math.max(0,inter_x2-inter_x1 +1);
+                    float h = Math.max(0,inter_y2-inter_y1+1);
 
-            float iou = (w * h) / (maxbox_area + box2_area);
+                    float iou = (w * h) / (maxbox_area + box2_area);
 
-            if(iou < 0.5){
-                preoutput2[k][i] = 0;
+                    if(iou > 0.49){
+                        class_score[i][k-5] = 0;
+                    }
+                    else{
+                        class_score[i][k-5] = preoutput2[k][i];
+                        Log.e("OTHER", String.valueOf(class_score[i][k-5]));
+                        next_max.add(i);
+                    }
+                }
             }
         }
+        return next_max;
     }
 
-    private float max(float[] arr) {
+    private int max(float[] arr) {
         int maxIndex = 0;
         float max = 0.0f;
         for (int i = 0; i < arr.length; i++) {
@@ -186,8 +215,7 @@ public class AfterDetectActivity extends AppCompatActivity {
                 maxIndex = i;
             }
         }
-        Log.e("MAX", String.valueOf(maxIndex)+' '+ String.valueOf(max));
-        return max;
+        return maxIndex;
     }
 
     private float[][] arr2arr2(float[] arr, int num) {
@@ -201,6 +229,7 @@ public class AfterDetectActivity extends AppCompatActivity {
                     result[j-2][i] = arr[k+j-4] - arr[k+j-2];
                     result[j-1][i] = arr[k+j-3] - arr[k+j-1];
                     conf = arr[k+j];
+                    result[j][i] = conf;
                 }
                 else{
                     result[j][i] = arr[k+j] * conf;
